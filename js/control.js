f234d79d10facdf701c6f9e4b9b5bccf1c45d862
nguyenhanhaha <155989915+nguyenhanhaha@users.noreply.github.com>
2024-01-22 13:27:43 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$('#cn').owlCarousel({
+  loop: true, //cho lap lai 
+  items: 1, // xuat hien 1 canh thoi
+  smartSpeed: 1000, //toc do thay doi anh
+  autoplay: true, //cho phep tu dong chay
+  autoplayTimeout: 5000, //thoi gian chờ khi chuyển ảnh khi chạy tự động 
+  nav: false, //cho xuất hiện bộ nút tới lui 
+  dots:true,
+})
